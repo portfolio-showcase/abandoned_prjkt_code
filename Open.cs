ae530a08d38740dc1ac22d221fc663f7dddd304d
prjkt-5 <63883816+prjkt-5@users.noreply.github.com>
2020-04-17 18:53:19 -0400

    Add files via upload

---
+++ Open.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open : MonoBehaviour
+{
+    [SerializeField]
+    GameObject switchOff;
+    [SerializeField]
+    GameObject switchOn;
+    //door starts closed
+    public bool isOn = false;
+    public bool OpenDoor = true;
+    private void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = switchOff.GetComponent<SpriteRenderer>().sprite;
+    }
+    //trigger sets door open
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = switchOn.GetComponent<SpriteRenderer>().sprite;
+
+        isOn = true;
+    }
+}
---
+++ Weapon_Pickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_Pickup : MonoBehaviour
+{
+    public GameObject[] weapons;
+    public GameObject weaponHere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponHere = weapons [Random.Range(0, weapons.Length)];
+        GetComponent<SpriteRenderer>().sprite = weaponHere.GetComponent<SpriteRenderer>().sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
